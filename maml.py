12766227c70fdf4679539463b523bffb7a029a8b
Akash <akashc@stanford.edu>
2020-11-14 04:30:44 +0000

    Add weighter network to MAML training framework -- test framework incomplete

--- maml.py
+++ maml.py
@@ -7,6 +7,7 @@ import pickle
 import random
 import pandas as pd
 
+from protonet import get_prototypes
 from data import DataGenerator
 from util import cross_entropy_loss, accuracy
 
@@ -59,14 +60,31 @@ class ConvLayers(tf.keras.layers.Layer):
         hidden2 = conv_block(hidden1, weights['conv2'], weights['b2'], self.bn2)
         hidden3 = conv_block(hidden2, weights['conv3'], weights['b3'], self.bn3)
         hidden4 = conv_block(hidden3, weights['conv4'], weights['b4'], self.bn4)
-        hidden4 = tf.reduce_mean(input_tensor=hidden4, axis=[1, 2])
+        hidden5 = tf.reduce_mean(input_tensor=hidden4, axis=[1, 2])
         
-        return tf.matmul(hidden4, weights['w5']) + weights['b5']
+        return tf.matmul(hidden5, weights['w5']) + weights['b5'], hidden5
+
+class Sampler(tf.keras.Model):
+    def __init__(self, hidden_dim, num_classes):
+        super(Sampler, self).__init__()
+        self.layer1 = tf.keras.layers.Dense(hidden_dim)
+        self.layer2 = tf.keras.layers.Dense(hidden_dim)
+        self.layer3 = tf.keras.layers.Dense(num_classes)
+        #self.num_unlabeled = num_unlabeled
+
+    def call(self, input):
+        x = self.layer1(input)
+        x = tf.keras.activations.relu(x)
+        x = self.layer2(x)
+        x = tf.keras.activations.relu(x)
+        x = self.layer3(x)
+        update_weights = tf.keras.activations.relu(x) # will be (n_way, k_shot, latent_dim)
+        return update_weights
 
 class MAML(tf.keras.Model):
     def __init__(self, dim_input=1, dim_output=1,
                 num_inner_updates=1,
-                inner_update_lr=0.4, num_filters=32, k_shot=5, learn_inner_update_lr=False):
+                inner_update_lr=0.4, num_filters=32, n_way=5, k_shot=5, learn_inner_update_lr=False):
         super(MAML, self).__init__()
         self.dim_input = dim_input
         self.dim_output = dim_output
@@ -75,7 +93,7 @@ class MAML(tf.keras.Model):
         self.dim_hidden = num_filters
         self.channels = 1
         self.img_size = int(np.sqrt(self.dim_input/self.channels))
-
+        self.k_shot = k_shot
         seed = 42
 
         # outputs_ts[i] and losses_ts_post[i] are the output and loss after i+1 inner gradient updates
@@ -91,6 +109,8 @@ class MAML(tf.keras.Model):
         # inner training loop
         tf.random.set_seed(seed)
         self.conv_layers = ConvLayers(self.channels, self.dim_hidden, self.dim_output, self.img_size)
+        hidden_dim = 16
+        self.swn = Sampler(hidden_dim, 1)
 
         self.learn_inner_update_lr = learn_inner_update_lr
 
@@ -103,9 +123,9 @@ class MAML(tf.keras.Model):
                                 name=f'inner_update_lr_{key}_{j}') for j in range(num_inner_updates)
                                                 ]
 
-    def call(self, inp, meta_batch_size=25, num_inner_updates=1):
+    def call(self, inp, meta_batch_size=25, num_inner_updates=1, num_combined_updates=1):
         def task_inner_loop(inp, reuse=True,
-                        meta_batch_size=25, num_inner_updates=1):
+                        meta_batch_size=25, num_inner_updates=1, num_combined_updates=1):
             """
                 Perform gradient descent for one task in the meta-batch (i.e. inner-loop).
                 Args:
@@ -121,7 +141,10 @@ class MAML(tf.keras.Model):
                 task_output: a list of outputs, losses and accuracies at each inner update
             """
             # the inner and outer loop data
-            input_tr, input_ts, label_tr, label_ts = inp
+            print(f"Inner loop input length: {len(inp)}")
+            input_tr, input_ts, unlabeled, label_tr, label_ts = inp
+            n_way = label_tr.shape[1]
+            num_unlabeled = unlabeled.shape[0]
 
             # weights corresponds to the initial weights in MAML (i.e. the meta-parameters)
             weights = self.conv_layers.conv_weights
@@ -135,13 +158,14 @@ class MAML(tf.keras.Model):
             # after i+1 inner gradient updates
             task_outputs_ts, task_losses_ts, task_accuracies_ts = [], [], []
 
-            with tf.GradientTape(persistent=True) as g:
+            with tf.GradientTape(persistent=True) as g1, tf.GradientTape(persistent=True) as g2:
+
 
                 # perform initial forward pass with meta-parameters 
-                initial_out_tr = self.conv_layers(input_tr, weights)
+                initial_out_tr, swn_input = self.conv_layers(input_tr, weights)
                 task_output_tr_pre = initial_out_tr
                 task_loss_tr_pre = self.loss_func(initial_out_tr, label_tr)
-                grads = g.gradient(task_loss_tr_pre, list(weights.values()))
+                grads = g1.gradient(task_loss_tr_pre, list(weights.values()))
 
                 # created copy of weights with first update
                 if self.learn_inner_update_lr:
@@ -152,7 +176,7 @@ class MAML(tf.keras.Model):
                                     for (var_name, var), grad in zip(weights.items(), grads)}
 
 
-                out_ts = self.conv_layers(input_ts, updated_weights)
+                out_ts, _ = self.conv_layers(input_ts, updated_weights)
                 loss_ts = self.loss_func(out_ts, label_ts)
                 task_outputs_ts.append(out_ts)
                 task_losses_ts.append(loss_ts)
@@ -160,56 +184,94 @@ class MAML(tf.keras.Model):
                 # iteratively update parameters (not meta-parameters)
                 for i in range(num_inner_updates - 1):
                     
-                    out_tr = self.conv_layers(input_tr, updated_weights)
+                    out_tr, _ = self.conv_layers(input_tr, updated_weights)
                     loss = self.loss_func(out_tr, label_tr)
 
-                    grads = g.gradient(loss, list(updated_weights.values()))
+                    grads = g1.gradient(loss, list(updated_weights.values()))
 
                     if self.learn_inner_update_lr:
                         updated_weights = {var_name: var - self.inner_update_lr_dict[var_name][i + 1] * grad
-                                    for (var_name, var), grad in zip(weights.items(), grads)}
+                                    for (var_name, var), grad in zip(updated_weights.items(), grads)}
                     else:
                         updated_weights = {var_name: var - self.inner_update_lr * grad
-                                    for (var_name, var), grad in zip(weights.items(), grads)}
+                                    for (var_name, var), grad in zip(updated_weights.items(), grads)}
 
-                    out_ts = self.conv_layers(input_ts, updated_weights)
+                    out_ts, _ = self.conv_layers(input_ts, updated_weights)
                     loss_ts = self.loss_func(out_ts, label_ts)
                     task_outputs_ts.append(out_ts)
                     task_losses_ts.append(loss_ts)
+                
+                out_unlabeled, unlabeled_embedding = self.conv_layers(unlabeled, updated_weights)
+                out_labeled, labeled_embedding = self.conv_layers(input_tr, updated_weights)
+                labeled_prototypes = get_prototypes(None, labeled_embedding, n_way, self.k_shot)
 
+                flattened_protos = tf.reshape(labeled_prototypes, (-1,)) #(num_classes * latent_dim,)
+                flattened_protos = tf.expand_dims(flattened_protos, axis=0)
+                flattened_protos = tf.tile(flattened_protos, multiples=[num_unlabeled, 1]) # (num_unlabeled, num_classes*latent_dim)
+       
+                weight_input = tf.concat([unlabeled_embedding, flattened_protos], axis=1)
+                loss_weights = self.swn(weight_input) # (num_unlabeled, n_way)
+
+                for _ in range(num_combined_updates):
+                    out_lab, _ = self.conv_layers(input_tr, updated_weights)
+                    out_unlab, _ = self.conv_layers(unlabeled, updated_weights)
+                    unlabeled_labels = tf.one_hot(tf.argmax(out_unlab, axis=1), n_way)
+                    lab_loss = self.loss_func(out_lab, label_tr)
+                    unlab_loss = self.loss_func(out_unlab, unlabeled_labels, weights=loss_weights)
+
+                    combined_loss = lab_loss + unlab_loss
+
+                    model_grads = g1.gradient(combined_loss, list(updated_weights.values()))
+                    if self.learn_inner_update_lr:
+                        updated_weights = {var_name: var - self.inner_update_lr_dict[var_name][i + 1] * grad
+                                    for (var_name, var), grad in zip(updated_weights.items(), model_grads)}
+                    else:
+                        updated_weights = {var_name: var - self.inner_update_lr * grad
+                                    for (var_name, var), grad in zip(updated_weights.items(), model_grads)}
+                
+                out_ts, _ = self.conv_layers(input_ts, updated_weights)
+                loss_ts = self.loss_func(out_ts, label_ts)
+                task_outputs_ts.append(out_ts)
+                task_losses_ts.append(loss_ts)
+        
+
+            
             # Compute accuracies from output predictions
             task_accuracy_tr_pre = accuracy(tf.argmax(input=label_tr, axis=1),
                                             tf.argmax(input=tf.nn.softmax(task_output_tr_pre), axis=1))
 
-            for j in range(num_inner_updates):
+            for j in range(num_inner_updates + 1):
                 task_accuracies_ts.append(accuracy(tf.argmax(input=label_ts, axis=1),
                                           tf.argmax(input=tf.nn.softmax(task_outputs_ts[j]), axis=1)))
+
 
             task_output = [task_output_tr_pre, task_outputs_ts, task_loss_tr_pre, task_losses_ts, task_accuracy_tr_pre, task_accuracies_ts]
 
             return task_output
 
-        input_tr, input_ts, label_tr, label_ts = inp
+        input_tr, input_ts, unlabeled, label_tr, label_ts = inp
         # to initialize the batch norm vars, might want to combine this, and not run idx 0 twice.
-        unused = task_inner_loop((input_tr[0], input_ts[0], label_tr[0], label_ts[0]),
+        unused = task_inner_loop((input_tr[0], input_ts[0], unlabeled[0], label_tr[0], label_ts[0]),
                             False,
                             meta_batch_size,
                             num_inner_updates)
-        out_dtype = [tf.float32, [tf.float32]*num_inner_updates, tf.float32, [tf.float32]*num_inner_updates]
-        out_dtype.extend([tf.float32, [tf.float32]*num_inner_updates])
+        out_dtype = [tf.float32, [tf.float32]*(num_inner_updates + 1), tf.float32, [tf.float32]*(num_inner_updates + 1)]
+        out_dtype.extend([tf.float32, [tf.float32]*(num_inner_updates + 1)])
         task_inner_loop_partial = partial(task_inner_loop,
                                         meta_batch_size=meta_batch_size,
                                         num_inner_updates=num_inner_updates)
         result = tf.map_fn(task_inner_loop_partial,
-                        elems=(input_tr, input_ts, label_tr, label_ts),
+                        elems=(input_tr, input_ts, unlabeled, label_tr, label_ts),
                         dtype=out_dtype,
                         parallel_iterations=meta_batch_size)
 
         return result
 
 
-def outer_train_step(inp, model, optim, meta_batch_size=25, num_inner_updates=1):
-    with tf.GradientTape(persistent=False) as outer_tape:
+def outer_train_step(inp, model, optim, meta_batch_size=25, num_inner_updates=1, num_combined_updates=1):
+
+    print("Models' trainable variables: ", len(model.trainable_variables))
+    with tf.GradientTape(persistent=False) as outer_tape_cls, tf.GradientTape(persistent=False) as outer_tape_swn:
         result = model(inp, meta_batch_size=meta_batch_size, num_inner_updates=num_inner_updates)
 
         outputs_tr, outputs_ts, losses_tr_pre, losses_ts, accuracies_tr_pre, accuracies_ts = result
@@ -217,8 +279,11 @@ def outer_train_step(inp, model, optim, meta_batch_size=25, num_inner_updates=1)
         total_losses_ts = [tf.reduce_mean(loss_ts) for loss_ts in losses_ts]
 
     # perform meta-update
-    gradients = outer_tape.gradient(total_losses_ts[-1], model.trainable_variables)
-    optim.apply_gradients(zip(gradients, model.trainable_variables))
+    cls_gradients = outer_tape_cls.gradient(total_losses_ts[-1], model.trainable_variables) # 
+    #swn_gradients = outer_tape_swn.gradient(total_losses_ts[-1], model.swn.trainable_variables)
+    print(f"Gradient length: {len(cls_gradients)}")
+    optim.apply_gradients(zip(cls_gradients, model.trainable_variables))
+    #optim.apply_gradients(zip(swn_gradients, model.swn.trainable_variables))
 
     total_loss_tr_pre = tf.reduce_mean(losses_tr_pre)
     total_accuracy_tr_pre = tf.reduce_mean(accuracies_tr_pre)
@@ -242,7 +307,7 @@ def outer_eval_step(inp, model, meta_batch_size=25, num_inner_updates=1):
 
 
 def meta_train_fn(model, exp_string, data_generator,
-               n_way=5, meta_train_iterations=15000, meta_batch_size=25,
+               n_way=5, meta_train_iterations=15000, meta_batch_size=25, num_unlabeled=1, num_combined_updates=1,
                log=True, logdir='../logs/', k_shot=1, num_inner_updates=1, meta_lr=0.001,
                output_filename=None):
     
@@ -250,6 +315,8 @@ def meta_train_fn(model, exp_string, data_generator,
     SAVE_INTERVAL = 100
     PRINT_INTERVAL = 10  
     TEST_PRINT_INTERVAL = PRINT_INTERVAL*5
+
+    
 
     pre_accuracies, post_accuracies = [], []
 
@@ -277,14 +344,16 @@ def meta_train_fn(model, exp_string, data_generator,
         
         new_shape_im = (meta_batch_size, n_way * k_shot, all_images.shape[-1])
         new_shape_lbl = (meta_batch_size, n_way * k_shot, n_way)
-        input_tr = tf.reshape(all_images[:, :, :k_shot, :], new_shape_im)
-        input_ts = tf.reshape(all_images[:, :, k_shot:, :], new_shape_im)
-        label_tr = tf.reshape(all_labels[:, :, :k_shot, :], new_shape_lbl)
-        label_ts = tf.reshape(all_labels[:, :, k_shot:, :], new_shape_lbl)
+        x = tf.reshape(all_images[:, :, :k_shot, :], new_shape_im)
+        q = tf.reshape(all_images[:, :, k_shot:2*k_shot, :], new_shape_im)
+        u = tf.reshape(all_images[:, :, 2*k_shot:, :], (meta_batch_size, n_way * num_unlabeled, all_images.shape[-1]))
+        x_label = tf.reshape(all_labels[:, :, :k_shot, :], new_shape_lbl)
+        q_label = tf.reshape(all_labels[:, :, k_shot:2*k_shot, :], new_shape_lbl)
 
-        inp = (input_tr, input_ts, label_tr, label_ts)
-        
-        result = outer_train_step(inp, model, optimizer, meta_batch_size=meta_batch_size, num_inner_updates=num_inner_updates)
+        inp = (x, q, u, x_label, q_label)
+        print(len(inp))
+
+        result = outer_train_step(inp, model, optimizer, meta_batch_size=meta_batch_size, num_inner_updates=num_inner_updates, num_combined_updates=num_combined_updates)
 
         if itr % SUMMARY_INTERVAL == 0:
             pre_accuracies.append(result[-2])
@@ -391,7 +460,7 @@ def meta_test_fn(model, data_generator, n_way=5, meta_batch_size=25, k_shot=1,
 
 def run_maml(n_way=5, k_shot=1, meta_batch_size=25, meta_lr=0.001,
              inner_update_lr=0.4, num_filters=32, num_inner_updates=1,
-             learn_inner_update_lr=False,
+             learn_inner_update_lr=False, num_unlabeled=5, num_combined_updates=5,
              resume=False, resume_itr=0, log=True, logdir='/tmp/data',
              data_path='../omniglot_resized',meta_train=True,
              meta_train_iterations=15000, meta_train_k_shot=-1,
@@ -399,7 +468,9 @@ def run_maml(n_way=5, k_shot=1, meta_batch_size=25, meta_lr=0.001,
 
 
     # call data_generator and get data with k_shot*2 samples per class
-    data_generator = DataGenerator(n_way, k_shot*2, n_way, k_shot*2, config={'data_folder': data_path})
+    data_generator = DataGenerator(n_way, k_shot*2 + num_unlabeled, n_way, k_shot*2 + num_unlabeled, config={'data_folder': data_path})
+
+    hidden_dim = 16
 
     # set up MAML model
     dim_output = data_generator.dim_output
@@ -412,6 +483,8 @@ def run_maml(n_way=5, k_shot=1, meta_batch_size=25, meta_lr=0.001,
                 num_filters=num_filters,
                 learn_inner_update_lr=learn_inner_update_lr)
 
+    
+
     if meta_train_k_shot == -1:
         meta_train_k_shot = k_shot
     if meta_train_inner_update_lr == -1:
@@ -419,12 +492,12 @@ def run_maml(n_way=5, k_shot=1, meta_batch_size=25, meta_lr=0.001,
 
     exp_string = 'cls_'+str(n_way)+'.mbs_'+str(meta_batch_size) + '.k_shot_' + str(meta_train_k_shot) + '.inner_numstep_' + str(num_inner_updates) + '.inner_updatelr_' + str(meta_train_inner_update_lr) + '.learn_inner_update_lr_' + str(learn_inner_update_lr)
 
-
+    #meta_batch_size=25, num_unlabeled, num_combined_updates,
     if meta_train:
         filename = output_file if output_file is not None else exp_string
         output = meta_train_fn(model, exp_string, data_generator,
-                    n_way, meta_train_iterations, meta_batch_size, log, logdir,
-                    k_shot, num_inner_updates, meta_lr, output_filename=filename)
+                    n_way, meta_train_iterations, meta_batch_size, num_unlabeled, num_combined_updates,
+                    log, logdir, k_shot, num_inner_updates, meta_lr, output_filename=filename)
         
         output.to_csv(filename)
 
--- util.py
+++ util.py
@@ -4,8 +4,15 @@ import tensorflow as tf
 
 
 
-def cross_entropy_loss(pred, label):
-    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=tf.stop_gradient(label)))
+def cross_entropy_loss(pred, label, weights=None):
+
+    if weights is None:
+      losses = tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=tf.stop_gradient(label)) # (b, n)
+      return tf.reduce_mean(losses)
+    
+    losses = tf.nn.sigmoid_cross_entropy_with_logits(logits=pred, labels=tf.stop_gradient(label))
+    return tf.multiply(losses, weights)
+
 
 def accuracy(labels, predictions):
   return tf.reduce_mean(tf.cast(tf.equal(labels, predictions), dtype=tf.float32))
